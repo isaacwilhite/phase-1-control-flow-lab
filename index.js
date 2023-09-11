f89440b5141766f70d2f149f7c94000794aaaf9a
Isaac Wilhite <isaacwilhite987@gmail.com>
2023-09-11 17:02:20 -0600

    solution

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+function scuberGreetingForFeet(rideLengthInt){
+  if (rideLengthInt <= 400) {
+    return"This one is on me!"
+  }
+  else if (rideLengthInt >= 400 && rideLengthInt <= 2000) {
+    return "That will be twenty bucks."
+  }
+  else if (rideLengthInt > 2000 && rideLengthInt <= 2500) {
+    return "I will gladly take your thirty bucks."
+  }
+  else if (rideLengthInt > 2500) {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(cityDestination){
+  if (cityDestination === "NYC") {
+    return "Ok, sounds good."
+  }
+    return 'No go.'
+  
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipString){
+  if (tipString === 'generous')
+  {return "Thank you so much."}
+  else if (tipString === 'not as generous') {
+    return "Thank you."
+  }
+  else {
+    return "Bye."
+  }
 }
